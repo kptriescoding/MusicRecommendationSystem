fcff640724e2605a99e83105b2d339fa6cdc0734
kptriescoding <karthikpai0*@gmail.com>
2022-09-07 14:22:44 +0530

    Recommendation System Update

--- src/main/java/RecommedationSystem/RecommenderSystem.java
+++ src/main/java/RecommedationSystem/RecommenderSystem.java
@@ -1,88 +1,87 @@
 package RecommedationSystem;
 import tech.tablesaw.api.*;
-import tech.tablesaw.columns.Column;
 import tech.tablesaw.selection.Selection;
 
 import java.util.*;
 import static tech.tablesaw.aggregate.AggregateFunctions.sum;
 
 public class RecommenderSystem extends MLTools{
-    private Table df_songs;
-    final private Table  df_songs_data, df_user;
-   final private int no_of_songs, no_of_similar_songs, no_of_users,no_of_search_songs;
+    private Table dfSongs;
+    final private Table dfSongsData, dfUser;
+   final private int noOfSongs, noOfSimilarSongs, noOfUsers, noOfSearchSongs;
     final private String songPath,userTablePath;
     public RecommenderSystem(String songPath,String userTablePath) {
         this.songPath=songPath;
         this.userTablePath=userTablePath;
-        this.df_songs = Table.read().csv(songPath);
-        this.df_songs_data = Table.create();
-        df_songs_data.addColumns(df_songs.column("SongId"));
-        df_songs_data.addColumns(df_songs.column("TotalFrequency"));
-        StringColumn input = df_songs.stringColumn("Artist");
+        this.dfSongs = Table.read().csv(songPath);
+        this.dfSongsData = Table.create();
+        dfSongsData.addColumns(dfSongs.column("SongId"));
+        dfSongsData.addColumns(dfSongs.column("TotalFrequency"));
+        StringColumn input = dfSongs.stringColumn("Artist");
         Integer[] encoded_output = tranform((ArrayList<String>) input.asList());
         IntColumn encoded_column=IntColumn.create("Artist",encoded_output);
-        df_songs_data.addColumns(encoded_column);
-        input = df_songs.stringColumn("Language");
+        dfSongsData.addColumns(encoded_column);
+        input = dfSongs.stringColumn("Language");
         encoded_output =tranform((ArrayList<String>) input.asList());
         encoded_column=IntColumn.create("Language",encoded_output);
-        df_songs_data.addColumns(encoded_column);
-        input = df_songs.stringColumn("Composer");
+        dfSongsData.addColumns(encoded_column);
+        input = dfSongs.stringColumn("Composer");
         encoded_output = tranform((ArrayList<String>) input.asList());
         encoded_column=IntColumn.create("Composer",encoded_output);
-        df_songs_data.addColumns(encoded_column);
-        input = df_songs.stringColumn("Album");
+        dfSongsData.addColumns(encoded_column);
+        input = dfSongs.stringColumn("Album");
         encoded_output = tranform((ArrayList<String>) input.asList());
         encoded_column=IntColumn.create("Album",encoded_output);
-        df_songs_data.addColumns(encoded_column);
-        this.df_user = Table.read().csv(userTablePath);
-        this.no_of_songs = 5;
-        this.no_of_users = 20;
-        this.no_of_similar_songs = 3;
-        this.no_of_search_songs=15;
+        dfSongsData.addColumns(encoded_column);
+        this.dfUser = Table.read().csv(userTablePath);
+        this.noOfSongs = 5;
+        this.noOfUsers = 20;
+        this.noOfSimilarSongs = 3;
+        this.noOfSearchSongs =15;
     }
 
     public Table findSimilarSongs(String song){
-    Row songRow=df_songs_data.where(df_songs_data.stringColumn("SongId").isEqualTo(song)).row(0);
+    Row songRow= dfSongsData.where(dfSongsData.stringColumn("SongId").isEqualTo(song)).row(0);
     ArrayList<Integer> rSongArr=convertRowToArray(songRow,songRow.columnCount());
     ArrayList<Integer> songArr;
-    Double[] cosineSimilarities=new Double[df_songs.rowCount()];
-            for (int i=0;i<df_songs.rowCount();i++){
-                Row song2=df_songs_data.row(i);
+    Double[] cosineSimilarities=new Double[dfSongs.rowCount()];
+            for (int i = 0; i< dfSongs.rowCount(); i++){
+                Row song2= dfSongsData.row(i);
                 songArr=convertRowToArray(song2,song2.columnCount());
             cosineSimilarities[i]=cosineSimilarity(rSongArr, songArr);
             }
         DoubleColumn cosineColumn=DoubleColumn.create("CosineSimilarity",cosineSimilarities);
-        df_songs.addColumns(cosineColumn);
-        Table returnSong = df_songs.sortOn("CosineSimilarity").first(no_of_similar_songs);
-        df_songs.removeColumns(cosineColumn);
+        dfSongs.addColumns(cosineColumn);
+        Table returnSong = dfSongs.sortOn("CosineSimilarity").first(noOfSimilarSongs);
+        dfSongs.removeColumns(cosineColumn);
         return returnSong;
 }
     public Table findSimilarUsers(String user) {
-        Row userRow=df_user.where(df_user.stringColumn("UserId").isEqualTo(user)).row(0);
+        Row userRow= dfUser.where(dfUser.stringColumn("UserId").isEqualTo(user)).row(0);
         ArrayList<Integer> rUserArr=convertRowToArray(userRow,userRow.columnCount());
         ArrayList<Integer> userArr;
-        Double[] cosineSimilarities=new Double[df_user.rowCount()];
-        for (int i=0;i<df_user.rowCount();i++){
-            Row user2=df_user.row(i);
+        Double[] cosineSimilarities=new Double[dfUser.rowCount()];
+        for (int i = 0; i< dfUser.rowCount(); i++){
+            Row user2= dfUser.row(i);
             userArr=convertRowToArray(user2,user2.columnCount());
             cosineSimilarities[i]=cosineSimilarity(rUserArr, userArr);
         }
         DoubleColumn cosineColumn=DoubleColumn.create("CosineSimilarity",cosineSimilarities);
-        df_user.addColumns(cosineColumn);
-        Table returnUsers=df_user.sortOn("CosineSimilarity").first(no_of_users);
-        df_user.removeColumns(cosineColumn);
+        dfUser.addColumns(cosineColumn);
+        Table returnUsers= dfUser.sortOn("CosineSimilarity").first(noOfUsers);
+        dfUser.removeColumns(cosineColumn);
         return returnUsers;
     }
     public void addNewRow(String user){
-        Row userRow=df_user.row(0);
+        Row userRow= dfUser.row(0);
         userRow.setString("UserId",user);
         for(int i=1;i<userRow.columnCount();i++)
             userRow.setInt(i,0);
-        df_user.append(userRow);
-        df_user.write().csv(userTablePath);
+        dfUser.append(userRow);
+        dfUser.write().csv(userTablePath);
     }
     public Table getRecommendation(String user) {
-        Table userTable=df_user.where(df_user.stringColumn("UserId").isEqualTo(user));
+        Table userTable= dfUser.where(dfUser.stringColumn("UserId").isEqualTo(user));
         if(userTable.rowCount()==0){
             addNewRow(user);
         }
@@ -98,7 +97,7 @@ public class RecommenderSystem extends MLTools{
         }
         ArrayList<String> songArr=new ArrayList<>();
         Iterator itr=topSongs.keySet().iterator();
-        while(itr.hasNext()&&songArr.size()<no_of_songs){
+        while(itr.hasNext()&&songArr.size()< noOfSongs){
             songArr.add(topSongs.get((Double) itr.next()));
         }
         Table reqSongs=null;
@@ -116,33 +115,33 @@ public class RecommenderSystem extends MLTools{
     }
     public void updateFrequency(String songId,String user){
         int index=-1;
-        for(int i=0;i<df_songs.rowCount();i++)
-            if (Objects.equals(df_songs.stringColumn("SongId").get(i), songId)){
+        for(int i = 0; i< dfSongs.rowCount(); i++)
+            if (Objects.equals(dfSongs.stringColumn("SongId").get(i), songId)){
                 index = i;
                 break;
             }
-        IntColumn tfCol = (IntColumn) df_songs.column("TotalFrequency");
+        IntColumn tfCol = (IntColumn) dfSongs.column("TotalFrequency");
         int tf= tfCol.get(index);
         tf+=1;
         tfCol.set(index,tf);
-        df_songs.replaceColumn("TotalFrequency",tfCol);
-        df_songs.write().csv(songPath);
-        for(int i=0;i<df_user.rowCount();i++)
-            if (Objects.equals(df_user.stringColumn("UserId").get(i), songId)){
+        dfSongs.replaceColumn("TotalFrequency",tfCol);
+        dfSongs.write().csv(songPath);
+        for(int i = 0; i< dfUser.rowCount(); i++)
+            if (Objects.equals(dfUser.stringColumn("UserId").get(i), songId)){
                 index = i;
                 break;
             }
-        IntColumn fCol = (IntColumn) df_user.column(songId);
+        IntColumn fCol = (IntColumn) dfUser.column(songId);
         int f= fCol.get(index);
         f+=1;
         fCol.set(index,f);
-        df_user.replaceColumn(songId,fCol);
-        df_user.write().csv(userTablePath);
+        dfUser.replaceColumn(songId,fCol);
+        dfUser.write().csv(userTablePath);
 
     }
     public Table searchBarRecommender(String search){
-        df_songs=df_songs.sortOn("-TotalFrequency");
-        StringColumn searchColumn=df_songs.stringColumn("SongName");
+        dfSongs = dfSongs.sortOn("-TotalFrequency");
+        StringColumn searchColumn= dfSongs.stringColumn("SongName");
         String temp;
         ArrayList<String> originalSongsName=new ArrayList<>();
         for(int i=0;i<searchColumn.size();i++){
@@ -154,11 +153,11 @@ public class RecommenderSystem extends MLTools{
         Selection searchFilter=searchColumn.startsWith(search);
         for(int i=0;i<searchColumn.size();i++)
             searchColumn.set(i,originalSongsName.get(i));
-        Table recommendedSongs=df_songs.where(searchFilter);
+        Table recommendedSongs= dfSongs.where(searchFilter);
         searchFilter=searchColumn.containsString(search);
-        recommendedSongs.append(df_songs.where(searchFilter));
-        recommendedSongs.append(df_songs);
-        return recommendedSongs.dropDuplicateRows().first(no_of_search_songs);
+        recommendedSongs.append(dfSongs.where(searchFilter));
+        recommendedSongs.append(dfSongs);
+        return recommendedSongs.dropDuplicateRows().first(noOfSearchSongs);
     }
     public static void main(String[] args){
         String songPath="src/main/java/RecommedationSystem/song_data.csv";
