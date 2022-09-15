4d217ba4ce97448259eb44c0d99a08547620ee63
dileep-shar <dilsharma0220@gmail.com>
2022-09-15 11:46:20 +0530

    some

--- src/main/java/com/example/music_recommendation_system/HelloApplication.java
+++ src/main/java/com/example/music_recommendation_system/HelloApplication.java
@@ -3,11 +3,15 @@ package com.example.music_recommendation_system;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -20,23 +24,25 @@ import java.util.ArrayList;
 public class HelloApplication extends Application {
     ImageView play_pause, next, prev;
     Button search;
+
     @Override
     public void start(Stage stage) throws IOException {
         Player p = new Player();
         p.fetchSongs("src/main/resources/songs");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        setUpStage(p, scene,stage,"home");
-
-
+        setUpStage(p, scene, stage, "home");
 
 
     }
 
-    static void setUpStage(Player p, Scene scene,Stage stage, String present_scene) {
+    static void setUpStage(Player p, Scene scene, Stage stage, String present_scene) {
 
         System.out.println(p.songs.size());
 //        boolean playing = false;
@@ -44,36 +50,51 @@ public class HelloApplication extends Application {
         play_pause = (ImageView) scene.lookup("#play_view");
         next = (ImageView) scene.lookup("#next_view");
         prev = (ImageView) scene.lookup("#prev_view");
-        File f1 = new File("src/main/resources/images/player/play.png");
-        Image image1 = new Image(f1.toURI().toString());
+        File playImage = new File("src/main/resources/images/player/play.png");
+        File pauseImage = new File("src/main/resources/images/player/pause.png");
 
+        Image image1 = new Image(playImage.toURI().toString());
+
+
+        GridPane songsList = (GridPane) scene.lookup("#songs_grid_pane");
+        songsList.setPadding(new Insets(10,10,10,10));
+        songsList.setHgap(10);
+        songsList.setVgap(10);
+
+        int cols=3, colCnt = 0, rowCnt = 0;
+        for(int i = 0;i<p.songs.size();i++){
+            songsList.add(new Label(p.songs.get(i)),colCnt,rowCnt);
+            colCnt++;
+            if (colCnt>cols) {
+                rowCnt++;
+                colCnt=0;
+            }
+        }
         play_pause.setImage(image1);
         play_pause.setOnMouseClicked(mouseEvent -> {
             if (p.isPlaying()) {
-
-                File f = new File("src/main/resources/images/player/play.png");
-                Image image = new Image(f.toURI().toString());
+                Image image = new Image(playImage.toURI().toString());
                 play_pause.setImage(image);
                 p.pause();
             } else {
                 p.play();
-                File f = new File("src/main/resources/images/player/pause.png");
-                Image image = new Image(f.toURI().toString());
+
+                Image image = new Image(pauseImage.toURI().toString());
                 play_pause.setImage(image);
             }
+            FlowPane f = new FlowPane();
 
         });
 
         next.setOnMouseClicked(mouseEvent -> {
-            File f = new File("src/main/resources/images/player/pause.png");
-            Image image = new Image(f.toURI().toString());
+            Image image = new Image(pauseImage.toURI().toString());
             play_pause.setImage(image);
             p.next();
             p.setAlbumArt();
         });
         prev.setOnMouseClicked(mouseEvent -> {
-            File f = new File("src/main/resources/images/player/pause.png");
-            Image image = new Image(f.toURI().toString());
+
+            Image image = new Image(pauseImage.toURI().toString());
             play_pause.setImage(image);
             p.prev();
         });
@@ -94,7 +115,7 @@ public class HelloApplication extends Application {
             stage.close();
             stage.setScene(scene1);
             stage.show();
-            setUpStage(p, scene1,stage,"search");
+            setUpStage(p, scene1, stage, "search");
         });
         home.setOnMouseClicked(mouseEvent -> {
             FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
@@ -108,7 +129,7 @@ public class HelloApplication extends Application {
             stage.close();
             stage.setScene(scene1);
             stage.show();
-            setUpStage(p, scene1,stage,"home");
+            setUpStage(p, scene1, stage, "home");
         });
 //        playlist.setOnMouseClicked(mouseEvent -> {
 //            FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("playlist.fxml"));
@@ -124,6 +145,11 @@ public class HelloApplication extends Application {
 //            stage.show();
 //            setUpStage(p, scene1,stage);
 //        });
+
+    }
+
+    static void SetPlayListScene(Player P, Stage stage, Scene scene) {
+
 
     }
 
--- src/main/java/com/example/music_recommendation_system/HelloController.java
+++ src/main/java/com/example/music_recommendation_system/HelloController.java
@@ -1,15 +1,44 @@
 package com.example.music_recommendation_system;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button home;
+
+    @FXML
+    private Button search;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected Scene onHomeButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        return scene;
+    }
+
+    @FXML
+    protected Scene onSearchButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("search.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        return scene;
+    }
+
+    @FXML
+    protected void onLikeButtonClick(){
+
     }
 }
